236b46c9a742cd365cf71defb8f40c82f95bc04c
Varun <msvarun021@gmail.com>
2026-05-28 14:31:15 +0530

    Force shorebird patch

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.spydex"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
